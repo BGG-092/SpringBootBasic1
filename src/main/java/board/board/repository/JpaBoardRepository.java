7a36c3f4f71f5c0127aac553a6307bce1b541b37
bark <bark@DESKTOP-BHCBH8L>
2019-08-05 22:33:29 +0900

    하 이노디비 셋팅실수....

--- src/main/java/board/board/repository/JpaBoardRepository.java
+++ src/main/java/board/board/repository/JpaBoardRepository.java
@@ -1,20 +1,18 @@
 package board.board.repository;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import board.board.entity.BoardEntity;
 import board.board.entity.BoardFileEntity;
 
-public interface JpaBoardRepository extends CrudRepository<BoardEntity, Integer> {
+public interface JpaBoardRepository extends CrudRepository<BoardEntity, Integer>{
 
 	List<BoardEntity> findAllByOrderByBoardIdxDesc();
-
-	@Query("SELECT file Form BoardFileENtity file where board_idx = :boardIdx AND idx = :idx")
+	
+	@Query("SELECT file FROM BoardFileEntity file WHERE board_idx = :boardIdx AND idx = :idx")
 	BoardFileEntity findBoardFile(@Param("boardIdx") int boardIdx, @Param("idx") int idx);
-
 }
